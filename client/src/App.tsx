9d7f19396aa5ceaf57f10ef5b9bfc7d77e76df56
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-15 07:21:45 -0300

    added react query data fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,37 @@
 import { Dialog } from './components/ui/dialog'
 import CreateGoal from './components/create-goal'
 import Summary from './components/summary'
-// import EmptyGoals from './components/empty-goals'
+import { useEffect, useState } from 'react'
+import EmptyGoals from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+
+type SummaryResponseType = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 
 export default function App() {
+  const { data } = useQuery<SummaryResponseType>({
+    queryKey: ['summary'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/summary')
+      const data = await response.json()
+
+      return data.summary
+    },
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
